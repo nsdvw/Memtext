5b75e5e5fd893cfdb6c9ad3a5ad37c30fc416e37
inside <inside@inside.com>
2016-03-31 01:32:05 +0300

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$settings['displayErrorDetails'] = true;
+$settings['db']['dbname'] = 'memtext';
+$settings['db']['user'] = 'root';
+$settings['db']['pass'] = '';
+$settings['db']['host'] = 'localhost';
+
+$app = new \Slim\App(['settings' => $settings]);
+
+$container = $app->getContainer();
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO(
+        "mysql:host={$db['host']};dbname={$db['dbname']}",
+        $db['user'],
+        $db['pass']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$container['view'] = new \Slim\Views\Twig('../templates');
+$container['yandexApiKey'] = 'trnsl.1.1.20160330T163001Z.d161a299772702fe.' .
+                                '0d436c4c1cfc1713dea2aeb9d9e3f2bebae02844';
+
+$app->get('/', function (Request $request, Response $response) {
+
+    return $response;
+});
+
+$app->run();

deb88a98aa3f0ac75d36f4c44566ccb819828a98
inside <inside@inside.com>
2016-04-05 03:24:55 +0300

    disable submit btn by default, hide removed words

--- web/js/dict-update-form.js
+++ web/js/dict-update-form.js
@@ -1,14 +1,17 @@
 $(function () {
+    var submit = $("#saveButton");
+    submit.prop("disabled", true);
     $(".dictionary-table").on("click", ".delete-button", function () {
+        submit.prop("disabled", false);
+        $(this).parents("tr").hide();
         var input = $("#removeFields");
         var data = input.val();
         data = JSON.parse(data);
         var word = $(this).parents("tr").children("td:first").text();
-        if (data.words === undefined) {
-            data.words = [];
+        if (data === undefined) {
+            data = [];
         }
-        data.words.push(word);
+        data.push(word);
         input.val(JSON.stringify(data));
-        console.log(input.val());
     });
 });

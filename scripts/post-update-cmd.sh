c5a47ad68e818531c33154e46003ae35a56a1e7f
inside <inside@inside.com>
2016-04-05 05:18:16 +0300

    add ckeditor(wysiwyg) and htmlpurifier packages

--- scripts/post-update-cmd.sh
+++ scripts/post-update-cmd.sh
@@ -7,3 +7,12 @@ for font in `ls vendor/twbs/bootstrap/dist/fonts/`
 do
   cp "vendor/twbs/bootstrap/dist/fonts/$font" "web/fonts/$font"
 done
+
+cp -r vendor/ckeditor/ckeditor/adapters web/ckeditor/adapters
+cp -r vendor/ckeditor/ckeditor/lang web/ckeditor/lang
+cp -r vendor/ckeditor/ckeditor/plugins web/ckeditor/plugins
+cp -r vendor/ckeditor/ckeditor/skins web/ckeditor/skins
+cp vendor/ckeditor/ckeditor/ckeditor.js web/ckeditor/ckeditor.js
+cp vendor/ckeditor/ckeditor/config.js web/ckeditor/config.js
+cp vendor/ckeditor/ckeditor/contents.css web/ckeditor/contents.css
+cp vendor/ckeditor/ckeditor/styles.js web/ckeditor/styles.js

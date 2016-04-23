b2e63416507d56490cac02820e3ab5149285eff9
inside <inside@inside.com>
2016-04-24 00:06:35 +0300

    fix post update script

--- scripts/post-update-cmd.sh
+++ scripts/post-update-cmd.sh
@@ -3,10 +3,7 @@ cp vendor/components/jquery/jquery.min.js web/vendor-js/jquery.min.js
 cp vendor/twbs/bootstrap/dist/css/bootstrap.min.css web/vendor-css/bootstrap.min.css
 cp vendor/twbs/bootstrap/dist/js/bootstrap.min.js web/vendor-js/bootstrap.min.js
 
-for font in `ls vendor/twbs/bootstrap/dist/fonts/`
-do
-  cp "vendor/twbs/bootstrap/dist/fonts/$font" "web/fonts/$font"
-done
+cp vendor/twbs/bootstrap/dist/fonts/* web/fonts/
 
 cp -r vendor/ckeditor/ckeditor/adapters web/ckeditor/adapters
 cp -r vendor/ckeditor/ckeditor/lang web/ckeditor/lang
